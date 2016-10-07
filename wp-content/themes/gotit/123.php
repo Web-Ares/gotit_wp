ca3d3e465ed23dcedb2d641463a398f7b20c2052
aleksandrnikolajcuk <deathstars111>
2016-10-07 16:54:15 +0300

    fonts

---
+++ wp-content/themes/gotit/123.php
@@ -0,0 +1,20 @@
+<link rel="shortcut icon" href="<?php echo DIRECT ?>/favicons/favicon.ico">
+<link rel="icon" sizes="16x16 32x32 64x64" href="<?php echo DIRECT ?>/favicons/favicon.ico">
+<link rel="icon" type="image/png" sizes="196x196" href="<?php echo DIRECT ?>/favicons/favicon-192.png">
+<link rel="icon" type="image/png" sizes="160x160" href="<?php echo DIRECT ?>/favicons/favicon-160.png">
+<link rel="icon" type="image/png" sizes="96x96" href="<?php echo DIRECT ?>/favicons/favicon-96.png">
+<link rel="icon" type="image/png" sizes="64x64" href="<?php echo DIRECT ?>/favicons/favicon-64.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo DIRECT ?>/favicons/favicon-32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo DIRECT ?>/favicons/favicon-16.png">
+<link rel="apple-touch-icon" href="<?php echo DIRECT ?>/favicons/favicon-57.png">
+<link rel="apple-touch-icon" sizes="114x114" href="<?php echo DIRECT ?>/favicons/favicon-114.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?php echo DIRECT ?>/favicons/favicon-72.png">
+<link rel="apple-touch-icon" sizes="144x144" href="<?php echo DIRECT ?>/favicons/favicon-144.png">
+<link rel="apple-touch-icon" sizes="60x60" href="<?php echo DIRECT ?>/favicons/favicon-60.png">
+<link rel="apple-touch-icon" sizes="120x120" href="<?php echo DIRECT ?>/favicons/favicon-120.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?php echo DIRECT ?>/favicons/favicon-76.png">
+<link rel="apple-touch-icon" sizes="152x152" href="<?php echo DIRECT ?>/favicons/favicon-152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo DIRECT ?>/favicons/favicon-180.png">
+<meta name="msapplication-TileColor" content="#FFFFFF">
+<meta name="msapplication-TileImage" content="/favicon-144.png">
+<meta name="msapplication-config" content="/browserconfig.xml">
